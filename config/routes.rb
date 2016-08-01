031451c2910e30e69e231813a43f327f165988d7
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-01 16:31:16 +0900

    Delete descriptions for users

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   get   '/prototypes/index' =>    'prototypes#index'
   get   '/prototypes/new'   =>    'prototypes#new'
   get   '/prototypes/show'  =>    'prototypes#show'
-  get   '/users/:id'        =>    'users#show'
-  get   '/users/:id/edit'   =>    'users#edit'
-  patch '/users/:id'        =>    'users#update'
 
 end
