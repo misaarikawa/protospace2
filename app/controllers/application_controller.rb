7426f9d322b81e8c524e20328110765eb045c8b8
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-02 21:01:05 +0900

    Add redirect for logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+　def after_sign_out_path_for(resource)
+    'prototypes#index'
+  end
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << [:name, :profile, :works, :member_of]
