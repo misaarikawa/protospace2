894a1994445959e027374e10b9e02f054678ccde
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-05 12:00:28 +0900

    Make association for prototypes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :prototypes
+  accepts_nested_attributes_for :prototypes
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
