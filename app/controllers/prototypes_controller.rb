799b1a68a24d5cfbfea74f96cc61b24012853948
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-05 12:02:40 +0900

    Define new & create prototype

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,2 +1,17 @@
 class PrototypesController < ApplicationController
+
+	def new
+		# @user = User.find(params[:user_id])
+		@prototype = Prototype.new
+	end
+
+	def create
+		Prototype.create(create_params)
+		redirect_to action: :index
+	end
+
+  private
+  def create_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
 end
