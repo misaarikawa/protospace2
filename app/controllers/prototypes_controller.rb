49b0544962d9f1e16fa50b7189b77b75d87694cf
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-06 14:29:03 +0900

    Add description about before_action, authenticate_user!, notice, if

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,25 +1,33 @@
 class PrototypesController < ApplicationController
+	before_action :authenticate_user!, only: :new, :create
+	before_action :set_prototype, only: :show
 
     def index
-    	@prototypes = Prototype.all
+      @prototypes = Prototype.all
     end
 
-    def show
-    	@prototype = Prototype.find(params[:id])
+    def show	
     end
 
 	def new
-		@prototype = Prototype.new
-		@prototype.prototype_images.build
+	  @prototype = Prototype.new
+	  @prototype.prototype_images.build
 	end
 
 	def create
-		Prototype.create(create_params)
-		redirect_to action: :index
+	  if Prototype.create(prototype_params)
+	    redirect_to action: :index, notice: '新しいプロトタイプを登録しました'
+	  else
+	    render 'new'
+	  end
 	end
 
   private
-  def create_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, prototype_images_attributes: [:id, :prototype_id, :content, :status]).merge(user_id: current_user.id)
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, prototype_images_attributes: [:id, :content, :status]).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+  	@prototype = Prototype.find(params[:id])
   end
 end
