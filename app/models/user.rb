5cbb068ef2f6f94b2c0fd37936fc7b047f16b4bd
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-06 14:31:11 +0900

    Delete description about nested prototype_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :prototypes
   has_many :prototype_images
-  accepts_nested_attributes_for :prototype_images
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
