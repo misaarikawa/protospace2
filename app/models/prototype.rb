8c2db2324eddcb2828e805fb9a66b320c5335c14
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-15 22:03:45 +0900

    Edit indents and add description about destroy & validation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,10 @@
 class Prototype < ActiveRecord::Base
-	belongs_to :user
-	has_many   :prototype_images
-	accepts_nested_attributes_for :prototype_images
+  belongs_to :user
+  has_many   :prototype_images
+  accepts_nested_attributes_for :prototype_images, allow_destroy: true
 
-	validates :title, presence: true
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true
 
 end
