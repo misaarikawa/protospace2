c375af975e3bc7cf0ef37b2c4bddc5989a74731e
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-19 17:52:32 +0900

    Edit indents

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,27 +1,27 @@
 class PrototypesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
-	before_action :set_prototype, only: :show
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_prototype, only: :show
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.all.includes(:prototype_images)
   end
 
   def show	
   end
 
-	def new
-	  @prototype = Prototype.new
-	  @prototype.prototype_images.build
-	end
+  def new
+    @prototype = Prototype.new
+    @prototype.prototype_images.build
+  end
 
-	def create
+  def create
     @prototype = Prototype.new(prototype_params)
-	  if @prototype.save
-	    redirect_to action: :index, notice: '新しいプロトタイプを登録しました'
-	  else
-	    render 'new'
-	  end
-	end
+    if @prototype.save
+      redirect_to action: :index, notice: '新しいプロトタイプを登録しました'
+    else
+      render 'new'
+    end
+  end
 
   private
   def prototype_params
