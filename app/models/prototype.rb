e89093a3dd185dbfd3050ef6031cd22146670d7a
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-19 18:00:41 +0900

    Put together of validates about prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,8 +3,6 @@ class Prototype < ActiveRecord::Base
   has_many   :prototype_images
   accepts_nested_attributes_for :prototype_images, reject_if: lambda { |attributes| attributes['content'].blank? }, allow_destroy: true
 
-  validates :title, presence: true
-  validates :catch_copy, presence: true
-  validates :concept, presence: true
+  validates :title, :catch_copy, :concept, presence: true
 
 end
