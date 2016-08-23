490bebaed94806be8a74f4249b9dd6ab875e71a1
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-23 22:46:03 +0900

    Setting default_image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "noimage.png"].compact.join('_'))
-  　'/assets/images/noimage.png'
+    'noimage.png'
     # "/images/fallback/" + [version_name, "noimage.png"].compact.join('_')
   end
 
