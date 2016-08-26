8c88873cffcfbd595fb3540a9ab818ee0bcd26dc
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-08-26 19:25:16 +0900

    Add option to prototype model for delete prototpe_images

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many   :prototype_images
+  has_many   :prototype_images, dependent: :delete_all
   accepts_nested_attributes_for :prototype_images, reject_if: lambda { |attributes| attributes['content'].blank? }, allow_destroy: true
 
   validates :title, 
