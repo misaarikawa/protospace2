56541d6cd9eae0eeedb9891b7fc8ab4c18fc8890
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-09-05 22:26:32 +0900

    Add association for like,comment to prototype model

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,11 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
+  has_many   :likes, dependent: :destroy
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
+  
+  has_many   :comments
   has_many   :prototype_images, dependent: :delete_all
   accepts_nested_attributes_for :prototype_images, reject_if: lambda { |attributes| attributes['content'].blank? }, allow_destroy: true
 
@@ -7,4 +13,11 @@ class Prototype < ActiveRecord::Base
   :catch_copy, 
   :concept, 
   presence: true
+
+  MAXMUN_IMAGE_NUM = 3
+  def set_sub_thumbnails
+    sub_images = prototype_images.sub
+    MAXMUN_IMAGE_NUM.times { |i| sub_images[i] ||= prototype_images.build(status: "sub") }
+      sub_images
+  end
 end
