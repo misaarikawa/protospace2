3bf6f298d8ceb6346783e368714133b238014a1b
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-09-06 11:54:55 +0900

    Set before_action to find prototype_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype, only: :create
+
   def create
-    @prototype = Prototype.find(params[:prototype_id])
     @prototype.comments.create(comment_params)
     @comments = Comment.where(prototype_id: params[:prototype_id])
   end
@@ -8,5 +9,9 @@ class CommentsController < ApplicationController
   private
   def comment_params
     params.require(:comment).permit(:article).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 end
