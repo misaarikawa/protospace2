e0afe89920d3e6ba5930d07aa8895685eddfff13
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-09-17 21:14:42 +0900

    Add routing popular & newest for prototypes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'prototypes#index'
+  root 'prototypes/popular#index#index'
 
   devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :prototypes do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
+  end
+  scope module: :prototypes do
+    resources :popular, :newest, only: :index
   end
 end
