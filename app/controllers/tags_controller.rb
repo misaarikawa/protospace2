b3c5aa29000146e9607d8fe186c1e4386aad85fb
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-09-22 22:07:09 +0900

    Edit method for tags_controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = ActsAsTaggableOn::Tag.most_used
   end
 
   def show
-    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    @tag =  ActsAsTaggableOn::Tag.find(params[:tag_name])
     @prototypes = Prototype.tagged_with(@tag.name)
   end
 end
