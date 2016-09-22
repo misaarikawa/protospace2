1ca97becd02d7232230af807190ed4da47d1b4d9
misa arikawa <misaarikawa@MacBook-Pro-misaarikawa.local>
2016-09-22 22:52:50 +0900

    Ad  description about condition for tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.most_used
+    @tags = ActsAsTaggableOn::Tag.most_used.where("name not ?", nil)
   end
 
   def show
-    @tag =  ActsAsTaggableOn::Tag.find(params[:tag_name])
+    @tag =  ActsAsTaggableOn::Tag.where(params[:tag_name])
     @prototypes = Prototype.tagged_with(@tag.name)
   end
 end
